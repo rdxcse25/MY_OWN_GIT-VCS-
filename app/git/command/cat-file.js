a692ff7090d6bc8b90b3dae843a5735790d97ac2
Rituraj Debnath <Intern_Rituraj.Debnath@stashfin.com>
2025-08-15 14:55:09 +0530

    stage-1 try 3

--- app/git/command/cat-file.js
+++ app/git/command/cat-file.js
@@ -2,32 +2,34 @@ const path = require("path");
 const fs = require("fs");
 const zlib = require("zlib");
 
-class CatFileCommand{
-    constructor(flag, commitSHA){
+class CatFileCommand {
+    constructor(flag, commitSHA) {
         this.flag = flag;
         this.commitSHA = commitSHA;
 
     }
-    execute(){
+    execute() {
         const flag = this.flag;
         const commitSHA = this.commitSHA;
-        switch(flag){
-            case "-p":{
+        switch (flag) {
+            case "-p": {
                 const folder = commitSHA.slice(0, 2);
                 const file = commitSHA.slice(2);
                 const completePath = path.join(process.cwd(), ".git", "objects", folder, file);
 
-                if(!fs.existsSync(completePath)){
+                if (!fs.existsSync(completePath)) {
                     throw new Error(`Not a valid object name ${commitSHA}`);
                 }
 
                 const fileContent = fs.readFileSync(completePath);
 
                 const outputBuffer = zlib.inflateSync(fileContent);
-                const output =  outputBuffer.toString();
-                process.stdout.write(output);
+                const output = outputBuffer.toString();
+                const nullIndex = output.indexOf('\0');
+                const content = output.slice(nullIndex + 1);
+                process.stdout.write(content);
             }
-            break;
+                break;
         }
     }
 }
